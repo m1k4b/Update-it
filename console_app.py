ed7eefb71776009b020e193e756eb8d0afffad69
m1k4b <mika.axel.bernard@gmail.com>
2026-04-08 18:56:12 +0200

    Added the 'edit' feature, and the ability to change the installation directory.

--- console_app.py
+++ console_app.py
@@ -34,46 +34,64 @@ while True:
     else:
         print()
 
-    choice = int(input("Do you want to download one or create one? 1 to download, 2 to create, 3 to delete, 4 to exit the program. "))
+    choice = int(input("Choose the action you want to perform. 1 to download, 2 to create, 3 to delete, 4 to edit, 5 to exit the program. "))
     if choice == 1:
-        modpack_index = int(input("Enter the modpack number: ")) - 1
+        modpack_index = int(input("Enter the modpack number: "))-1
         modpack_to_download = data["modpacks"][modpack_index]
         version = input("Enter the version: ")
+        location = input("Enter the path to the directory where you want to install the modpack (d for .minecraft/mods): ")
+        if location.startswith("\""):
+            location = location[1:-1]
         print("Downloading, please wait...")
         for mod in modpack_to_download["mods"]:
-            status_bool, related_data = fast_download_mod(mod["name"], version, "fabric")
+            status_bool,related_data = fast_download_mod(mod["name"],version,"fabric",loc=None if location=="d" else location)
             if not status_bool:
                 status_to_msg = {
-                    "versionNotSupported": f"version {version} is not supported.",
-                    "loaderNotSupported": f"Fabric loader is not supported.",
-                    "notFound": f"an unknown error occurred. Please try again later."
+                    "versionNotSupported":f"version {version} is not supported.",
+                    "loaderNotSupported":f"the Fabric loader is not supported.",
+                    "notFound":f"an unknown error occurred. Please try again later."
                 }
                 print(f"Error while downloading {mod['name']}: {status_to_msg[related_data]}")
             else:
                 print(f"Successfully downloaded {mod['name']}! Saved at \"{related_data}\".")
 
-        print("Download completed!")
-        data["modpacks"][modpack_index]["downloads"] += 1
+        print("Download complete!")
+        data["modpacks"][modpack_index]["downloads"]+=1
         save()
-    elif choice == 2:
+    elif choice==2:
         mods = []
         modpack_name = input("Enter the name of the new modpack: ")
         while True:
-            mod_name = input("Enter the name of a mod you want to add (type 's' to finish): ")
-            if mod_name == "s":
+            mod_name = input("Enter the name of a mod you want to add (s to finish): ")
+            if mod_name=="s":
                 break
             mods.append({
-                "name": mod_name
+                "name":mod_name
             })
         data["modpacks"].append({
             "name": modpack_name,
             "mods": mods,
-            "downloads": 0
+            "downloads":0
         })
         save()
-    elif choice == 3:
+    elif choice==3:
         modpack_index = int(input("Enter the modpack number: ")) - 1
         data["modpacks"].pop(modpack_index)
         save()
-    elif choice == 4:
+    elif choice==4:
+        modpack_index = int(input("Enter the modpack number: ")) - 1
+        edit_choice = int(input("Enter 1 to change the name, 2 to add mods. "))
+        if edit_choice==1:
+            new_name = input("Enter the new name: ")
+            data["modpacks"][modpack_index]["name"] = new_name
+            save()
+        elif edit_choice==2:
+            while True:
+                mod_name = input("Enter the name of a mod you want to add (s to finish): ")
+                if mod_name == "s":
+                    break
+                data["modpacks"][modpack_index]["mods"].append({"name":mod_name})
+            save()
+    elif choice==5:
+        save()
         sys.exit()
